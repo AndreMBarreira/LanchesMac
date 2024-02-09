92b26c79c60d5a6d36c118362443cd35335843a2
AndreMBarreira <andremilla.am@gmail.com>
2024-02-09 13:27:55 -0300

    DbContext and migration

--- LanchesMac/Migrations/20240118185030_PopularLanches.cs
+++ LanchesMac/Migrations/20240118185030_PopularLanches.cs
@@ -13,7 +13,7 @@ namespace LanchesMac.Migrations
             migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl,ImagemUrl,IsLanchePreferido,Nome,Preco)" +
                 "VALUES(1,'Pão, presunto, mussarela e tomate','Delicioso pão francês quentinho na chapa com presunto e mussarela',1,'http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg','http://www.macoratti.net/Imagens/lanches/mistoquente4.jpg',0,'Misto quente', 8.00)");
             migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl,ImagemUrl,IsLanchePreferido,Nome,Preco)" +
-                "VALUES(1,'Pão, hambúrger, presunto, mussarela e batata palha','Pão de hamburger de nossa preparação,1,'http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg','http://www.macoratti.net/Imagens/lanches/cheeseburger.jpg',0,'Cheeseburger', 11.00)");
+                "VALUES(1,'Pão, hambúrger, presunto, mussarela e batata palha','Pão de hamburger de nossa preparação',1,'http://www.macoratti.net/Imagens/lanches/cheeseburger1.jpg','http://www.macoratti.net/Imagens/lanches/cheeseburger.jpg',0,'Cheeseburger', 11.00)");
             migrationBuilder.Sql("INSERT INTO Lanches(CategoriaId,DescricaoCurta,DescricaoDetalhada,EmEstoque,ImagemThumbnailUrl,ImagemUrl,IsLanchePreferido,Nome,Preco)" +
                 "VALUES(2,'Pão Integral, queijo branco, peito de peru, cenoura, alface, iogurte','Pão integral natural com queijo branco, peito de peru e cenoura ralada',1,'http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg','http://www.macoratti.net/Imagens/lanches/lanchenatural.jpg',1,'Lanche natural', 9.50)");
         }
