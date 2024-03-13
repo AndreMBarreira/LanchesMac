2a970110923ce198329a7a27c8f768048609022b
AndreMBarreira <andremilla.am@gmail.com>
2024-03-13 14:24:56 -0300

    View Pedido and PedidoController

--- LanchesMac/Models/Pedido.cs
+++ LanchesMac/Models/Pedido.cs
@@ -8,32 +8,32 @@ namespace LanchesMac.Models
         [Key]
         public int PedidoId { get; set; }
 
-        [Required(ErrorMessage = "Informe o {1}")]
+        [Required(ErrorMessage = "Informe o {0}")]
         [StringLength(50)]
         [Display(Name ="Nome")]
         public string Nome { get; set; }
 
-        [Required(ErrorMessage ="Informe o {1}")]
+        [Required(ErrorMessage ="Informe o {0}")]
         [StringLength(50)]
         [Display(Name = "Sobrenome")]
         public string Sobrenome { get; set; }
 
-        [Required(ErrorMessage = "Informe o {1}")]
+        [Required(ErrorMessage = "Informe o {0}")]
         [Display(Name = "CEP")]
         [StringLength(8)]
         public string Cep { get; set; }
-        [Required(ErrorMessage = "Informe o {1}")]
+        [Required(ErrorMessage = "Informe o {0}")]
         [StringLength(100)]
         public string Logradouro { get; set; }
         [StringLength(50)]
         public string? Complemento { get; set; }
-        [Required(ErrorMessage = "Informe o {1}")]
+        [Required(ErrorMessage = "Informe o {0}")]
         [StringLength(100)]
         public string Bairro { get; set; }
         [StringLength(100)]
         [Display(Name = "Cidade")]
         public string Localidade { get; set; }
-        [Required(ErrorMessage = "Informe o {1}")]
+        [Required(ErrorMessage = "Informe o {0}")]
         [StringLength(2)]
         [Display(Name = "UF")]
         public string Uf { get; set; }
@@ -43,7 +43,7 @@ namespace LanchesMac.Models
         [DataType(DataType.PhoneNumber)]
         public string Telefone { get; set; }
 
-        [Required(ErrorMessage = "Informe o email")]
+        //[Required(ErrorMessage = "Informe o email")]
         [StringLength(50)]
         [DataType(DataType.EmailAddress)]
         [RegularExpression(@"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])",
