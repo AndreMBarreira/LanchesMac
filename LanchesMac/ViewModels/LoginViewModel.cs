93a20d9cb79e50953efac73945d8570030be823c
AndreMBarreira <andremilla.am@gmail.com>
2024-03-19 08:02:05 -0300

    LoginViewModel and AccountController

---
+++ LanchesMac/ViewModels/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace LanchesMac.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Informe o {0}")]
+        [Display(Name = "Usuário")]
+        public string UserName;
+        [Required(ErrorMessage = "Informe o {0}")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password;
+        public string ReturnUrl;
+    }
+}
