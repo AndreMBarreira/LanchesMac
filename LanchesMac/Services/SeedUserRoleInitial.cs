7d5cb0d764c2bd08afa5ed2b3c2672ba3b3c79cb
AndreMBarreira <andremilla.am@gmail.com>
2024-03-24 09:55:08 -0300

    Seeding Roles

--- LanchesMac/Services/SeedUserRoleInitial.cs
+++ LanchesMac/Services/SeedUserRoleInitial.cs
@@ -1,6 +1,44 @@
-﻿namespace LanchesMac.Services
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LanchesMac.Services
 {
-    public class SeedUserRoleInitial
+    public class SeedUserRoleInitial : ISeedUserRoleInitial
     {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public SeedUserRoleInitial(UserManager<IdentityUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void SeedRoles()
+        {
+            if(!_roleManager.RoleExistsAsync("Member").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "Member";
+                role.NormalizedName = "MEMBER";
+                IdentityResult roleResult = _roleManager.CreateAsync(role).Result;
+            }
+            if (!_roleManager.RoleExistsAsync("Admin").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "Admin";
+                role.NormalizedName = "ADMIN";
+                IdentityResult roleResult = _roleManager.CreateAsync(role).Result;
+            }
+        }
+
+        public void SeedRoles()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SeedUsers()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
